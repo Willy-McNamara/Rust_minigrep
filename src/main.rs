e7b2c227490629e7bab568e89764fcc63dc8da70
Willy McNamara <willy.mcnamara@moodys.com>
2024-11-26 12:06:07 -0700

    Splits off standard output and error output

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,12 @@ fn main() {
     
     
     let config = Config::build(&args).unwrap_or_else(|err| {
-	println!("problem parsing arguments: {err}");
+	eprintln!("problem parsing arguments: {err}");
 	process::exit(1);
     });
 
     if let Err(e) = minigrep::run(config) {
+	eprintln!("Application error: {e}");
 	process::exit(1);
     }
 }
